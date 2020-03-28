12a360e591e614333348a95af81fbecaedf032ad
Xinyu Zhou <zxytim@gmail.com>
2020-03-29 00:12:59 +0800

    add basic tests

--- Makefile
+++ Makefile
@@ -1,4 +1,17 @@
 all:
 
+style-check:
+	black --diff --check pdf2images bin tests
+	flake8 --ignore E501,E203,F401,W503,W504 --radon-max-cc 13 pdf2images bin tests
+
+test:
+	pytest \
+	    --cov=pdf2images \
+	    --no-cov-on-fail \
+	    --cov-report=html:htmlcov \
+	    --cov-report term \
+	    --doctest-modules \
+	    pdf2images tests
+
 wheel:
 	python3 setup.py sdist bdist_wheel
---
+++ tests/test_pdf2images.py
@@ -0,0 +1,20 @@
+import unittest
+import pdf2images
+import os
+
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+ASSETS_DIR = os.path.join(THIS_DIR, 'assets')
+
+
+class TestPDF2Images(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.pdf_path = os.path.join(ASSETS_DIR, 'Sequence Modeling 2019-04.pdf')
+        with open(cls.pdf_path, 'rb') as f:
+            cls.pdf_data = f.read()
+
+    def test_basics(self):
+        self.assertEqual(
+            pdf2images.get_num_pages_given_path(self.pdf_path),
+            175)
